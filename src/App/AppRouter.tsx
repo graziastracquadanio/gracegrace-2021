e43d231542729ca3ec3a22ab06dafeb9530a1c2b
graziastracquadanio <graziastracquadanio@gmail.com>
2022-03-24 18:36:43 +0100

    redirect to about page

--- src/App/AppRouter.tsx
+++ src/App/AppRouter.tsx
@@ -1,6 +1,6 @@
 import React, { lazy, useEffect } from 'react';
 
-import { Routes, Route, useLocation } from 'react-router-dom';
+import { Routes, Route, useLocation, Navigate } from 'react-router-dom';
 
 const AboutPageComponent = lazy(() => import('views/AboutPage'));
 const ContactPageComponent = lazy(() => import('views/ContactPage'));
@@ -20,7 +20,6 @@ export const AppRouter = () => {
 
   return (
     <Routes>
-      <Route path="/" element={<AboutPageComponent />} />
       <Route path="/login" element={<LoginPageComponent />} />
       <Route path="/styleguide" element={<StyleguidePageComponent />} />
       <Route path="/about" element={<AboutPageComponent />} />
@@ -30,6 +29,7 @@ export const AppRouter = () => {
       <Route path="/recipes/new" element={<RecipeEditPageComponent />} />
       <Route path="/recipes/:recipeId" element={<RecipePageComponent />} />
       <Route path="/recipes/:recipeId/edit" element={<RecipeEditPageComponent />} />
+      <Route path="/" element={<Navigate replace to="/about" />} />
       <Route path="*" element={<NotFoundPageComponent />} />
     </Routes>
   );
