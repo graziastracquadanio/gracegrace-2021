bdb5378d434db2da13054012789226f7d6c20c54
graziastracquadanio <graziastracquadanio@gmail.com>
2022-03-24 16:14:39 +0100

    fix: scroll smooth to top on route change

--- src/App/AppRouter.tsx
+++ src/App/AppRouter.tsx
@@ -1,6 +1,6 @@
-import React, { lazy } from 'react';
+import React, { lazy, useEffect } from 'react';
 
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 
 const AboutPageComponent = lazy(() => import('views/AboutPage'));
 const ContactPageComponent = lazy(() => import('views/ContactPage'));
@@ -12,18 +12,25 @@ const ResumePageComponent = lazy(() => import('views/ResumePage'));
 const StyleguidePageComponent = lazy(() => import('views/StyleguidePage'));
 const RecipeEditPageComponent = lazy(() => import('views/RecipeEditPage'));
 
-export const AppRouter = () => (
-  <Routes>
-    <Route path="/" element={<AboutPageComponent />} />
-    <Route path="/login" element={<LoginPageComponent />} />
-    <Route path="/styleguide" element={<StyleguidePageComponent />} />
-    <Route path="/about" element={<AboutPageComponent />} />
-    <Route path="/about/resume" element={<ResumePageComponent />} />
-    <Route path="/contact" element={<ContactPageComponent />} />
-    <Route path="/recipes" element={<RecipesListPageComponent />} />
-    <Route path="/recipes/new" element={<RecipeEditPageComponent />} />
-    <Route path="/recipes/:recipeId" element={<RecipePageComponent />} />
-    <Route path="/recipes/:recipeId/edit" element={<RecipeEditPageComponent />} />
-    <Route path="*" element={<NotFoundPageComponent />} />
-  </Routes>
-);
+export const AppRouter = () => {
+  const location = useLocation();
+  useEffect(() => {
+    window.scroll({ top: 0, behavior: 'smooth' });
+  }, [location]);
+
+  return (
+    <Routes>
+      <Route path="/" element={<AboutPageComponent />} />
+      <Route path="/login" element={<LoginPageComponent />} />
+      <Route path="/styleguide" element={<StyleguidePageComponent />} />
+      <Route path="/about" element={<AboutPageComponent />} />
+      <Route path="/about/resume" element={<ResumePageComponent />} />
+      <Route path="/contact" element={<ContactPageComponent />} />
+      <Route path="/recipes" element={<RecipesListPageComponent />} />
+      <Route path="/recipes/new" element={<RecipeEditPageComponent />} />
+      <Route path="/recipes/:recipeId" element={<RecipePageComponent />} />
+      <Route path="/recipes/:recipeId/edit" element={<RecipeEditPageComponent />} />
+      <Route path="*" element={<NotFoundPageComponent />} />
+    </Routes>
+  );
+};
